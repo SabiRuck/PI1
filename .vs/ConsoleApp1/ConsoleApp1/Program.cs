a06fa77cd6549d839ef97bfc3abc9d3ee04a6655
Sabi Ruckova <sabinka.ruckova@gmail.com>
2024-09-27 13:36:51 +0200

    first

---
+++ .vs/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            char operacia;
+
+            a = Convert.ToInt32(Console.ReadLine());
+            operacia = Convert.ToChar(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+
+            if (operacia == '+')
+                Console.Write("{0}+{1}={2}\n", a, b, (a + b));
+            else if (operacia == '-')
+                Console.Write("{0}-{1}={2}\n", a, b, (a - b));
+            else if (operacia == '*')
+                Console.Write("{0}*{1}={2}\n", a, b, (a * b));
+            else if (operacia == '/')
+                Console.Write("{0}/{1}={2}\n", a, b, (a / b));
+            else
+                Console.Write("WTF?!\n");
+        }
+    }
+}
