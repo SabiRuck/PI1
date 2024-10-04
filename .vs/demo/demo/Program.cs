ebec5c090673b7443ee82c544b870f0257ac23e1
Sabi Ruckova <sabinka.ruckova@gmail.com>
2024-10-04 13:23:39 +0200

    jdawf

---
+++ .vs/demo/demo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            char operacia;
+
+            a = Convert.ToInt32(Console.ReadLine());
+            operacia = Convert.ToChar(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+
+            if (operacia == '+')
+                Console.Write("{0}+{1}={2}\n", a, b, (a + b));
+            else if (operacia == '-')
+                Console.Write("{0}-{1}={2}\n", a, b, (a - b));
+            else if (operacia == '*')
+                Console.Write("{0}*{1}={2}\n", a, b, (a * b));
+            else if (operacia == '/')
+                Console.Write("{0}/{1}={2}\n", a, b, (a / b));
+            else
+                Console.Write("WTF?!\n");
+
+
+        }
+    }
+}
